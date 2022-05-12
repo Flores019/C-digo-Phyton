7a9d4aaff67cbb535928f37f81b0c189ec7f75e1
Flores019 <102966870+Flores019@users.noreply.github.com>
2022-05-12 17:52:09 -0400

    Create Stained Glass Window.py

---
+++ Stained Glass Window.py
@@ -0,0 +1,23 @@
+Rect(0, 275, 400, 125, fill='silver')
+Rect(110, 70, 180, 205, fill='saddleBrown')
+Rect(145, 105, 30, 40, fill='lightSkyBlue', border='black')
+Rect(225, 105, 30, 40, fill='lightSkyBlue', border='black')
+Rect(145, 165, 30, 40, fill='lightSkyBlue', border='black')
+Rect(225, 165, 30, 40, fill='lightSkyBlue', border='black')
+Rect(177, 215, 45, 60, fill='darkRed')
+Rect(0, 0, 400, 400, fill=None, border='wheat', borderWidth=40)
+
+Rect(40, 40, 320, 320, fill=None, border='black', borderWidth=10)
+
+# Draw the stained glass window.
+### (HINT: There should be nine rectangles.)
+### Place Your Code Here ###
+Rect(50, 50, 100, 100, fill='red', opacity=50, border='black')
+Rect(150, 50, 100, 100, fill='yellow', opacity=50, border='black')
+Rect(250, 50, 100, 100, fill='darkturquoise', opacity=50, border='black')
+Rect(50, 150, 100, 100, fill='yellow', opacity=50, border='black')
+Rect(150, 150, 100, 100, fill='darkturquoise', opacity=50, border='black')
+Rect(250, 150, 100, 100, fill='dodgerblue', opacity=50, border='black')
+Rect(50, 250, 100, 100, fill='darkturquoise', opacity=50, border='black')
+Rect(150, 250, 100, 100, fill='dodgerblue', opacity=50, border='black')
+Rect(250, 250, 100, 100, fill='blueviolet', opacity=50, border='black')
