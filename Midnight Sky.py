4d1ecb589c9a1ffd7978e2403cd1753356ec78f4
Flores019 <102966870+Flores019@users.noreply.github.com>
2022-05-12 17:51:02 -0400

    Create Midnight Sky.py

---
+++ Midnight Sky.py
@@ -0,0 +1,8 @@
+Circle(50, 50, 35, fill=None, opacity=30, border='white' )
+Circle(50, 50, 30, fill=gradient('peru', 'wheat'))
+Circle(150, 100, 25, fill=gradient('crimson', 'lightSalmon', start='bottom-left'), opacity=70)
+Line(100, 200, 275, 125, lineWidth=3, fill=gradient('black', 'white', start='right'))
+Circle(250, 250, 60, fill=gradient('black', 'grey', start='left'))
+Oval(150, 275, 100, 50, fill=gradient('black', 'grey', start='left'), opacity=90)
+Oval(125, 300, 100, 50, fill=gradient('black', 'grey', start='left'), opacity=90)
+Oval(200, 300, 100, 50, fill=gradient('black', 'grey', start='right'), opacity=90)
